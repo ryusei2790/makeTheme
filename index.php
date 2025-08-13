a853bd860fe5023c5cc4704fa8cf3b6fdae9df65
ryusei2790 <ryu.chn1978@gmail.com>
2025-08-13 14:07:37 +0900

    Add service and about sections to index.php for improved company representation

--- index.php
+++ index.php
@@ -61,6 +61,68 @@ get_header();
         </div>
     </div>
 </section>
+<section class="service-area flex flex-wrap justify-between">
+    <figure class="img flipLeftTrigger"><img src="img/pict_03.jpg" alt=""></figure>
+    <div class="content flipLeftTrigger">
+        <div class="content-area">
+        <h3><span>Development</span>受託開発事業</h3>
+        <p>プロジェクト/プロダクトマネージメントの観点からのご支援を致します。</p>
+        <div class="btn"><a href="#" class="btnlinestretches3"><span>Detail</span></a></div>
+        </div>
+    </div>
+    </section>
+    <section class="service-area flex flex-wrap justify-between">
+    <figure class="img flipLeftTrigger"><img src="img/pict_04.jpg" alt=""></figure>
+    <div class="content flipLeftTrigger">
+        <div class="content-area">
+        <h3><span>Education</span>教育事業</h3>
+        <p>エンジニアやデザイナーの養成、新人研修・プログラミング研修などを行っています。</p>
+        <div class="btn"><a href="#" class="btnlinestretches3"><span>Detail</span></a></div>
+        </div>
+    </div>
+    </section>
+	</section>
+    
+<section id="about" class="scroll-point">
+    <h2><span class="bgextend bgLRextendTrigger"><span class="bgappearTrigger">About</span></span></h2>
+    <ul class="about-list fadeUpTrigger">
+        <li>
+        <dl>
+            <dt>会社名</dt><dd>ハピカム株式会社</dd>
+        </dl>
+        </li>
+        <li>
+        <dl>
+            <dt>設立</dt><dd>2020年7月</dd>
+        </dl>
+        </li>
+        <li>
+        <dl>
+            <dt>資本金</dt><dd>2000万円</dd>
+        </dl>
+        </li>
+        <li>
+        <dl>
+            <dt>代表</dt><dd>久保田涼子</dd>
+        </dl>
+        </li>
+        <li>
+        <dl>
+            <dt>所在地</dt><dd>〒104-0000 東京都中央区1丁目1番1号</dd>
+        </dl>
+        </li>
+        <li>
+        <dl>
+            <dt>電話</dt><dd>03-1234-5678</dd>
+        </dl>
+        </li>
+        <li>
+        <dl>
+            <dt>主な取引先</dt><dd>杉山商事<br>Y&amp;YCorporation<br>コテツ株式会社</dd>
+        </dl>
+        </li>
+    </ul>
+</section>
 </main><!-- #main -->
 
 <?php
