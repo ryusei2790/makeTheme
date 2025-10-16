4e4700b6500a7cb9e50ec5fc55cff05e5c097343
ryusei2790 <ryu.chn1978@gmail.com>
2025-10-16 16:35:24 +0900

    Update company information in page-about.php
    - Removed outdated details and replaced with current data for establishment date, capital, representative, location, phone number, and main business partners.
    - Ensured the page reflects accurate and up-to-date company information.

--- page-about.php
+++ page-about.php
@@ -15,32 +15,32 @@ get_header();
         </li>
         <li>
         <dl>
-            <dt>設立</dt><dd>2020年7月</dd>
+            <dt>設立</dt><dd></dd>
         </dl>
         </li>
         <li>
         <dl>
-            <dt>資本金</dt><dd>2000万円</dd>
+            <dt>資本金</dt><dd>50万円</dd>
         </dl>
         </li>
         <li>
         <dl>
-            <dt>代表</dt><dd>久保田涼子</dd>
+            <dt>代表</dt><dd>久保昌幹</dd>
         </dl>
         </li>
         <li>
         <dl>
-            <dt>所在地</dt><dd>〒104-0000 東京都中央区1丁目1番1号</dd>
+            <dt>所在地</dt><dd>東京都渋谷区渋谷</dd>
         </dl>
         </li>
         <li>
         <dl>
-            <dt>電話</dt><dd>03-1234-5678</dd>
+            <dt>電話</dt><dd>0</dd>
         </dl>
         </li>
         <li>
         <dl>
-            <dt>主な取引先</dt><dd>杉山商事<br>Y&amp;YCorporation<br>コテツ株式会社</dd>
+            <dt>主な取引先</dt><dd></dd>
         </dl>
         </li>
     </ul>
